62201f4824a53f04bbf1e4b38cb9144a66dfe8ae
an-nikol <voynskaa@gmail.com>
2024-05-19 13:12:01 +0300

    added solutions

---
+++ project_multilevel_inheritance/project/car.py
@@ -0,0 +1,6 @@
+from project_1.vehicle import Vehicle
+
+
+class Car(Vehicle):
+    def drive(self):
+        return "driving..."
---
+++ project_multilevel_inheritance/project/sports_car.py
@@ -0,0 +1,7 @@
+from project_1.car import Car
+
+
+class SportsCar(Car):
+
+    def race(self):
+        return "racing..."
