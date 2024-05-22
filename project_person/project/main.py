1054e0414bab5be8c70c0a3912f2a8ddbd2f1d7a
an-nikol <voynskaa@gmail.com>
2024-05-22 23:58:42 +0300

    added solutions

---
+++ project_person/project/main.py
@@ -0,0 +1,8 @@
+from project_1.person import Person
+from project_1.child import Child
+
+person = Person("Peter", 25)
+child = Child("Peter Junior", 5)
+print(person.name)
+print(person.age)
+print(child.__class__.__bases__[0].__name__)
