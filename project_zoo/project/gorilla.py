d4e7d8760832215efe49fed23a90e1c76d310b7b
an-nikol <voynskaa@gmail.com>
2024-06-08 23:35:15 +0300

    added solutions

---
+++ project_zoo/project/gorilla.py
@@ -0,0 +1,6 @@
+from project_1.mammal import Mammal
+
+
+class Gorilla(Mammal):
+    def __init__(self, name: str):
+        super().__init__(name)
---
+++ project_zoo/project/snake.py
@@ -0,0 +1,6 @@
+from project_1.reptile import Reptile
+
+
+class Snake(Reptile):
+    def __init__(self, name: str):
+        super().__init__(name)
